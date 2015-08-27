e01e3911be7f2661afbff13a3302c137e4bf779a
MaxMEllon <mozi_kke@excite.co.jp>
2015-08-27 23:50:08 +0900

    update

--- .bashrc
+++ .bashrc
@@ -3,3 +3,5 @@ mesg n
 source ~/.dotfiles/.aliases.bash
 source ~/.dotfiles/.prompt.bash
 source ~/.dotfiles/.exports.bash
+source /home/maxmellon/.evm/scripts/evm
+test -s "$HOME/.kiex/scripts/kiex" && source "$HOME/.kiex/scripts/kiex"
--- .exports.bash
+++ .exports.bash
@@ -3,6 +3,9 @@ export PATH=/usr/local/bin:${PATH}
 export PATH=${HOME}/.rbenv/bin:${PATH}
 export PATH=/opt/rh/qt48/root/usr/lib64/qt4/bin:${PATH}
 export PAGER=less
+export GOPATH=~/local/go
+export PATH=~/local/go/bin/:${PATH}
+export PATH=/usr/local/heroku/bin:${PATH}
 export DOTFONTPATH=${HOME}/share/fonts
 if [ `which rbenv` ]; then
   eval "$(rbenv init -)"
