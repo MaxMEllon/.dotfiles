20699f29b0d6cb123dab9485c31fea32e81149d9
MaxMEllon <mozi_kke@excite.co.jp>
2015-08-31 09:09:41 +0900

    go path

--- .exports.bash
+++ .exports.bash
@@ -2,7 +2,10 @@ export PATH=${HOME}/local/bin:${PATH}
 export PATH=/usr/local/bin:${PATH}
 export PATH=${HOME}/.rbenv/bin:${PATH}
 export PATH=/opt/rh/qt48/root/usr/lib64/qt4/bin:${PATH}
+export PATH=${HOME}/local/go/bin:${PATH}
 export PAGER=less
+export GISTY_DIR=${HOME}/gisty
+export GOPATH=${HOME}/local/go
 export DOTFONTPATH=${HOME}/share/fonts
 if [ `which rbenv` ]; then
   eval "$(rbenv init -)"
