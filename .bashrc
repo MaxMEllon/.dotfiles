d18e91ec6ad063048fdd10c7158419e8e0a27ae3
MaxMEllon <mozi_kke@excite.co.jp>
2015-09-03 00:54:19 +0900

    update

--- .bashrc
+++ .bashrc
@@ -3,5 +3,4 @@ mesg n
 source ~/.dotfiles/.aliases.bash
 source ~/.dotfiles/.prompt.bash
 source ~/.dotfiles/.exports.bash
-source /home/maxmellon/.evm/scripts/evm
 test -s "$HOME/.kiex/scripts/kiex" && source "$HOME/.kiex/scripts/kiex"
--- etc/bin/deploy.sh
+++ etc/bin/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for file in .gitconfig .minttyrc .tmux.d/.tmux.conf .zsh.d/.zshrc .inputrc .bashrc .pryrc
+for file in .gitconfig .gitignore_global .minttyrc .tmux.d/.tmux.conf .zsh.d/.zshrc .inputrc .bashrc .pryrc
 do
   ln -s .dotfiles/$file ~/
 done
