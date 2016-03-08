33e7a187d21dc04085a6ec88040dc5aa1370f21a
MaxMEllon <mozi_kke@excite.co.jp>
2016-03-09 06:45:39 +0900

    Remove No means check in tmux/init.sh

--- tmux/init.sh
+++ tmux/init.sh
@@ -6,11 +6,6 @@ set -eu
 
 source $1/lib/conveni.sh
 
-if ! has "tmux"; then
-  error_message 'PLEASE INSTALL BASH'
-  exit 1
-fi
-
 rm -rf ~/.tmux.conf
 ln -s $1/tmux/.tmux.conf ~/.tmux.conf > /dev/null
 exit $?
