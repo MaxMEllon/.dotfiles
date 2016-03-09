bd18158b4faa6abc9e551492e5868f9d3e1bd71c
MaxMEllon <mozi_kke@excite.co.jp>
2016-03-09 20:43:42 +0900

    Support run to rbenv by zsh

--- bash/env.bash
+++ bash/env.bash
@@ -7,7 +7,11 @@ export GISTY_DIR=${HOME}/gisty
 export GOPATH=${HOME}/local/go
 export PATH=/usr/local/heroku/bin:${PATH}
 export DOTFONTPATH=${HOME}/share/fonts
+# See: http://qiita.com/ma2saka/items/f975fff5af6d48255e0a
 if [ `which rbenv` ]; then
+  if $(which comparguments 2>/dev/null | grep 'built-in command' > /dev/null); then
+    eval "$(rbenv init - zsh)"
+  fi
   eval "$(rbenv init -)"
 fi
 test -s "$HOME/.kiex/scripts/kiex" && source "$HOME/.kiex/scripts/kiex"
