e24e9f1d982036320448a3f6f66ccd7797b32e56
MaxMEllon <maxmellon1994@gmail.com>
2016-10-19 15:00:00 +0900

    :bug: [zsh] fix cant use `-d` option in git-checkout-branch functions

--- zsh/functions/git-checkout-branch.zsh
+++ zsh/functions/git-checkout-branch.zsh
@@ -2,9 +2,16 @@ has fzf-tmux || return 0
 
 git-checkout-branch()
 {
-  local selected_branch=$(git branch | fzf-tmux | tr -d ' ' | tr -d '*')
-
-  git checkout $selected_branch
+  if (( $# == 0 )); then
+    local selected_branch=$(git branch | fzf-tmux | tr -d ' ' | tr -d '*')
+    git checkout $selected_branch
+  else
+    if [[ $1 == "-b" ]]; then
+      git checkout $1 $2
+    else
+      git checkout $1
+    fi
+  fi
 }
 
 alias gco='git-checkout-branch'
