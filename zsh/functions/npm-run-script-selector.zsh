65e696d1cacaa2222d45fe34ae5e377d244bc7f2
MaxMEllon <maxmellon1994@gmail.com>
2016-10-19 15:44:54 +0900

    :sparkles: [zsh] use yarn in `npm-run-scripts-selector()` if does exist yarn.

--- zsh/functions/npm-run-script-selector.zsh
+++ zsh/functions/npm-run-script-selector.zsh
@@ -5,7 +5,11 @@ npm-run-scripts-selector()
 {
   [ -s package.json ] || return 0
   command=$(cat package.json | jq ".scripts | keys | .[]" | tr -d '"' | fzf-tmux)
-  eval "npm run $command"
+  if has yarn; then
+    eval "yarn run $command"
+  else
+    eval "npm run $command"
+  fi
 }
 
 alias nrs="npm-run-scripts-selector"
