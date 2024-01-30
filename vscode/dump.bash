997a6fd9ada87f178e9d8eb378185bffce28bf49
MaxMEllon <maxmellon1994@gmail.com>
2024-01-30 17:26:31 +0900

    update

--- vscode/dump.bash
+++ vscode/dump.bash
@@ -4,4 +4,7 @@ set -eu
 
 echo "#!/bin/bash"
 
+echo "defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false"
+echo "defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false"
+
 code --list-extensions | xargs -L 1 echo code --install-extension
