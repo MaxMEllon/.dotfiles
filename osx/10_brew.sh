a154f3d777a9392d5969f85a6eac9e8271b7c871
MaxMEllon <maxmellon1994@gmail.com>
2024-12-11 15:10:34 +0900

    update

--- osx/10_brew.sh
+++ osx/10_brew.sh
@@ -38,19 +38,19 @@ _brew_install() {
 
 _brew_install wget
 _brew_install git
-_brew_install zsh
-_brew_install tmux
-_brew_install go
-_brew_install python
-_brew_install python3
-_brew_install rbenv
-_brew_install ruby-build
+# _brew_install zsh
+# _brew_install tmux
+# _brew_install go
+# _brew_install python
+# _brew_install python3
+# _brew_install rbenv
+# _brew_install ruby-build
 # _brew_tap "peco/peco" && _brew_install peco
-_brew_install fzf
-_brew_install tig
+# _brew_install fzf
+# _brew_install tig
 _brew_tap "motemen/ghq" && _brew_install ghq
-_brew_install rsense
-_brew_install ag
+# _brew_install rsense
+# _brew_install ag
 
 wait
 
